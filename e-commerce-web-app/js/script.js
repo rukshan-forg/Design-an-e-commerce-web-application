885ca487b2de65c1aeed27828e8479e717904654
rukshanprog <rukshanprog@gmail.com>
2023-07-31 10:17:15 +0530

    v 5

--- e-commerce-web-app/js/script.js
+++ e-commerce-web-app/js/script.js
@@ -23,4 +23,47 @@ initializeSidebar();
 
 
 
+// Advanced-seaech dropdown
+var myButton = document.getElementById("Advanced-seaech");
+var dropDownButton = document.getElementById("Advanced-seaech-dropdown");
+var openDropdown = false;
+myButton.addEventListener("click", main);
+
+function main() {
+      if (openDropdown == false) {
+            dropDownButton.style.display = "block";
+            dropDownButton.style.marginTop = "20px";
+            openDropdown = true;
+      } else {
+            dropDownButton.style.display = "none";
+            openDropdown = false;
+            dropDownButton.style.marginTop = "0px";
+      }
+}
+
+
+
+
+// shoping cart
+var shopingCartIcon = document.getElementById("shoping-cart-icon");
+var shopingCart = document.getElementById("shoping-cart");
+var openCartDropdown = false;
+shopingCartIcon.addEventListener("click", main2);
+
+function main2() {
+      if (openCartDropdown == false) {
+        shopingCart.style.display = "block";
+            openCartDropdown = true;
+      } else {
+        shopingCart.style.display = "none";
+            openCartDropdown = false;
+      }
+}
+
+
+
+
+
+
+
 
